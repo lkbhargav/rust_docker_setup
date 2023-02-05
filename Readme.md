ba8a70a6a73ac89ed1f7c4a79c64a1a0b9126c9a
lkbhargav <lkbhargav9@gmail.com>
2023-02-05 18:08:25 -0500

    Updated some parts to make it super dynamic

--- Readme.md
+++ Readme.md
@@ -3,10 +3,11 @@
 How to use this,
 1. Generate RSA key files by running the following,
     `ssh-keygen`
-2. Make sure to move the generated key files to `builder` directory
-3. Configure `.env` file with all required env variables (refer to `.unset_env` for required envs)
-4. Set the env variables by sourcing it (ex: `. .env`)
-5. Finally, build the docker image with your app binary using `make build`
+2. Make sure to move the generated key files (`id_rsa` files) to `builder` directory
+3. Add the public key to your Github SSH keys, so that cloning from container will work without hiccups
+4. Configure `.env` file with all required env variables (refer to `.unset_env` for required envs)
+5. Set the env variables by sourcing it (ex: `. .env` or `source .env`)
+6. Finally, build the docker image with your app binary using `make build`
 
 References:
 https://www.baeldung.com/ops/docker-memory-limit
--- build.sh
+++ build.sh
@@ -3,17 +3,27 @@
 repo_name=${REPOSITORY_NAME}
 current_working_dir=$(pwd)
 app_image_name=${APP_IMAGE_NAME}
+repo_link=${REPOSITORY_LINK}
+branch_to_clone=${BRANCH_TO_CLONE}
 
 docker rmi $app_image_name
 
-docker build --no-cache --build-arg REPOSITORY_NAME=$repo_name -t builder -f ./builder/Dockerfile ./builder
+docker build \
+  --no-cache \
+  --build-arg REPOSITORY_NAME=$repo_name \
+  --build-arg REPOSITORY_LINK=$repo_link \
+  --build-arg BRANCH_TO_CLONE=$branch_to_clone \
+  -t builder \
+  -f ./builder/Dockerfile ./builder
 
-# we don't want to run this container in detach mode
-docker run -m 2g --memory-reservation=512m --cpus=2 --cpu-shares=2000 -v $current_working_dir/bin:/app/bin/ --name builder_app builder
+# we don't want to run this container in detach mode to make sure everything happens synchronously
+docker run \
+  -m 2g --memory-reservation=512m \
+  --cpus=2 --cpu-shares=2000 \
+  -v $current_working_dir/bin:/app/bin/ \
+  --name builder_app builder
 
 docker rm builder_app
 docker rmi builder
-
-echo "APP IMAGE NAME: $app_image_name"
 
 docker build -t $app_image_name -f ./app/Dockerfile ./bin
